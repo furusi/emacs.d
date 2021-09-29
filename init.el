5947cf8e789847ea0bb075b26fa652735fafd528
Hiroki Furusho <furusityofu@gmail.com>
2021-09-29 15:48:52 +0900

    consult-fd関数を追加

--- init.el
+++ init.el
@@ -621,6 +621,31 @@
     :hook
     (completion-list-mode-hook . consult-preview-at-point-mode)
     :config
+    ;; https://github.com/minad/consult/wiki#find-files-using-fd
+    (defvar consult--fd-command nil)
+    (defun consult--fd-builder (input)
+      (unless consult--fd-command
+        (setq consult--fd-command
+              (if (eq 0 (call-process-shell-command "fdfind"))
+                  "fdfind"
+                "fd")))
+      (pcase-let* ((`(,arg . ,opts) (consult--command-split input))
+                   (`(,re . ,hl) (funcall consult--regexp-compiler
+                                          arg 'extended)))
+        (when re
+          (list :command (append
+                          (list consult--fd-command
+                                "--color=never" "--full-path"
+                                (consult--join-regexps re 'extended))
+                          opts)
+                :highlight hl))))
+
+    (defun consult-fd (&optional dir initial)
+      (interactive "P")
+      (let* ((prompt-dir (consult--directory-prompt "Fd" dir))
+             (default-directory (cdr prompt-dir)))
+        (find-file (consult--find (car prompt-dir) #'consult--fd-builder initial))))
+
     (consult-customize
      consult-theme
      :preview-key '(:debounce 0.2 any)
