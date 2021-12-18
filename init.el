239b0bdf53777ebac9e1ae208d8c98a1b21cbcbb
Hiroki Furusho <furusityofu@gmail.com>
2021-12-18 20:12:04 +0900

    カーソルの表示をboxに変更

--- init.el
+++ init.el
@@ -28,7 +28,6 @@
     (backup-directory-alist . '((".*" . "~/.ehist")))
     (byte-compile-warnings . '(cl-functions))
     (comment-style . 'multi-line)
-    (cursor-type . 'bar)
     (custom-theme-directory . ,(concat user-emacs-directory "themes/")) ;; テーマのディレクトリを設定
     (default-frame-alist .'((width . 200) (height . 50)))
     (dired-dwim-target . t)
