1bd133dafc0e73e4bca479c1d74b784d13be0bc3
Hiroki Furusho <furusityofu@gmail.com>
2021-12-23 17:46:13 +0900

    swap number of keyboard layout to symbol of it

--- init.el
+++ init.el
@@ -159,7 +159,27 @@
    (:isearch-mode-map
     ("C-o" . isearch-exit))))
 
-
+(leaf special-characer-mode
+  :url "https://github.com/madanh/special-characer-mode"
+  :config
+  (defmacro ins-val (val)
+  `(lambda () (interactive) (insert ,val)))
+  (define-minor-mode special-char-mode
+    "Toggle Special Character mode"
+    nil
+    " SpecialChar"
+    `(
+      (,(kbd "1") . ,(ins-val "!")) (,(kbd "!") . ,(ins-val "1")) (,[kp-1] . ,(ins-val "1"))
+      (,(kbd "2") . ,(ins-val "@")) (,(kbd "@") . ,(ins-val "2")) (,[kp-2] . ,(ins-val "2"))
+      (,(kbd "3") . ,(ins-val "#")) (,(kbd "#") . ,(ins-val "3")) (,[kp-3] . ,(ins-val "3"))
+      (,(kbd "4") . ,(ins-val "$")) (,(kbd "$") . ,(ins-val "4")) (,[kp-4] . ,(ins-val "4"))
+      (,(kbd "5") . ,(ins-val "%")) (,(kbd "%") . ,(ins-val "5")) (,[kp-5] . ,(ins-val "5"))
+      (,(kbd "6") . ,(ins-val "^")) (,(kbd "^") . ,(ins-val "6")) (,[kp-6] . ,(ins-val "6"))
+      (,(kbd "7") . ,(ins-val "&")) (,(kbd "&") . ,(ins-val "7")) (,[kp-7] . ,(ins-val "7"))
+      (,(kbd "8") . ,(ins-val "*")) (,(kbd "*") . ,(ins-val "8")) (,[kp-8] . ,(ins-val "8")) (,[kp-multiply] . ,(ins-val "*"))
+      (,(kbd "9") . ,(ins-val "(")) (,(kbd "(") . ,(ins-val "9")) (,[kp-9] . ,(ins-val "9"))
+      (,(kbd "0") . ,(ins-val ")")) (,(kbd ")") . ,(ins-val "0")) (,[kp-0] . ,(ins-val "0"))
+      )))
 
 (when (equal system-type 'darwin)
   (setq ns-command-modifier (quote meta))
