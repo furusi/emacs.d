e8a571795fc93ac1d0a8e06c7c21209262f9893d
Hiroki Furusho <furusityofu@gmail.com>
2022-01-13 13:24:57 +0900

    my:set-frame-*: show present value

--- lisp/misc.el
+++ lisp/misc.el
@@ -1,14 +1,14 @@
 (defun my:set-frame-width ()
   (interactive)
-  (set-frame-width (selected-frame) (my:set-frame--prompt "frame width")))
+  (set-frame-width (selected-frame) (my:set-frame--prompt "frame width" (frame-width))))
 (defun my:set-frame-height ()
   (interactive)
-  (set-frame-height (selected-frame) (my:set-frame--prompt "frame height")))
+  (set-frame-height (selected-frame) (my:set-frame--prompt "frame height" (frame-height))))
 
-(defun my:set-frame--prompt (prompt-message)
+(defun my:set-frame--prompt (prompt-message present-value)
   (if (window-system)
       (let ((width (string-to-number
-                    (read-from-minibuffer (format "%s: " prompt-message)))))
+                    (read-from-minibuffer (format "%s(present: %s): " prompt-message present-value)))))
         (when (< width 1)
           (error (format "%s must be number is bigger than 0: %s" prompt-message width)))
         width)
