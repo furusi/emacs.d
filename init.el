cab0dd60b76ad6c3dc8529de2944be38dde2b9ac
Hiroki Furusho <furusityofu@gmail.com>
2022-07-24 09:31:16 +0900

    add japanese-holidays

--- init.el
+++ init.el
@@ -389,6 +389,27 @@
   :straight t
   :require t
   )
+
+(leaf japanese-holidays
+  :doc "Calendar functions for the Japanese calendar"
+  :req "emacs-24.1" "cl-lib-0.3"
+  :tag "calendar" "emacs>=24.1"
+  :url "https://github.com/emacs-jp/japanese-holidays"
+  :emacs>= 24.1
+  :straight t
+  :require t
+  :config
+  (setq calendar-holidays ; 他の国の祝日も表示させたい場合は適当に調整
+        (append japanese-holidays holiday-local-holidays holiday-other-holidays))
+  (setq calendar-mark-holidays-flag t)	; 祝日をカレンダーに表示
+  ;; 土曜日・日曜日を祝日として表示する場合、以下の設定を追加します。
+  ;; デフォルトで設定済み
+  (setq japanese-holiday-weekend '(0 6)	   ; 土日を祝日として表示
+        japanese-holiday-weekend-marker	   ; 土曜日を水色で表示
+        '(holiday nil nil nil nil nil japanese-holiday-saturday))
+  (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+  (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend))
+
 (leaf magit
   :bind (("C-x g" . magit-status)
          (:magit-diff-mode-map
