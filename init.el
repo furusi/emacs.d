744b9613b695ddf94aaa83eb92915ae8b593eff4
Hiroki Furusho <furusityofu@gmail.com>
2022-10-23 07:28:42 +0900

    use eglot in emacs 29

--- init.el
+++ init.el
@@ -1193,8 +1193,10 @@ and `clavis-org-refile-refiled-from-header' variables."
   :req "emacs-26.1" "rust-mode-1.0.3" "dash-2.13.0" "f-0.18.2" "let-alist-1.0.4" "markdown-mode-2.3" "project-0.3.0" "s-1.10.0" "seq-2.3" "spinner-1.7.3" "xterm-color-1.6"
   :tag "languages" "emacs>=26.1"
   :emacs>= 26.1
-  :custom ((rustic-lsp-server . 'rust-analyzer)
-           (rustic-lsp-client . 'lsp-mode))
+  :custom `((rustic-lsp-server . 'rust-analyzer)
+            ,(if (string> emacs-version "29")
+                 '(rustic-lsp-client . 'eglot)
+               '(rustic-lsp-client . 'lsp-mode)))
   :bind
   ((:rustic-mode-map
     ("C-<return>" . default-indent-new-line)))
