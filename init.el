605230261aad2697fe5c80537a49556368f034ab
Hiroki Furusho <furusityofu@gmail.com>
2022-11-06 15:41:38 +0900

    add snippet

--- init.el
+++ init.el
@@ -1068,6 +1068,7 @@ n,SPC -next diff      |     h -highlighting       |  d -copy both to C
     :req "emacs-26.1" "rust-mode-1.0.3" "dash-2.13.0" "f-0.18.2" "let-alist-1.0.4" "markdown-mode-2.3" "project-0.3.0" "s-1.10.0" "seq-2.3" "spinner-1.7.3" "xterm-color-1.6"
     :tag "languages" "emacs>=26.1"
     :emacs>= 26.1
+    :require t
     :custom `((rustic-lsp-server . 'rust-analyzer)
               ,(if (string> emacs-version "29")
                    '(rustic-lsp-client . 'eglot)
