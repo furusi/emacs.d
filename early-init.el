a97b86bcac929051fa90f365e7c3af69ab82c363
Hiroki Furusho <furusityofu@gmail.com>
2022-12-07 11:35:06 +0900

    change elpaca initialize code

--- early-init.el
+++ early-init.el
@@ -44,33 +44,38 @@
 (custom-set-variables '(straight-vc-git-default-clone-depth 150))
 
 ;;elpaca
-(declare-function elpaca-generate-autoloads "elpaca")
 (defvar elpaca-directory (format "%spackages/%s/elpaca/" (expand-file-name user-emacs-directory) emacs-version))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(when-let ((elpaca-repo (expand-file-name "repos/elpaca/" elpaca-directory))
-           (elpaca-build (expand-file-name "elpaca/" elpaca-builds-directory))
-           (elpaca-target (if (file-exists-p elpaca-build) elpaca-build elpaca-repo))
-           (elpaca-url  "https://www.github.com/progfolio/elpaca.git")
-           ((add-to-list 'load-path elpaca-target))
-           ((not (file-exists-p elpaca-repo)))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :build (:not elpaca--activate-package)))
+(when-let ((repo (expand-file-name "repos/elpaca/" elpaca-directory))
+           (build (expand-file-name "elpaca/" elpaca-builds-directory))
+           (order (cdr elpaca-order))
+           ((add-to-list 'load-path (if (file-exists-p build) build repo)))
+           ((not (file-exists-p repo)))
            (buffer (get-buffer-create "*elpaca-bootstrap*")))
   (condition-case-unless-debug err
-      (progn
-        (unless (zerop (call-process "git" nil buffer t "clone" elpaca-url elpaca-repo))
-          (error "%s" (list (with-current-buffer buffer (buffer-string)))))
-        (byte-recompile-directory elpaca-repo 0 'force)
-        (require 'elpaca)
-        (elpaca-generate-autoloads "elpaca" elpaca-repo)
-        (kill-buffer buffer))
+      (if-let (((pop-to-buffer buffer '((display-buffer-reuse-window
+                                         display-buffer-same-window))))
+               ((zerop (call-process "git" nil buffer t "clone"
+                                     (plist-get order :repo) repo)))
+               (default-directory repo)
+               ((zerop (call-process "git" nil buffer t "checkout"
+                                     (or (plist-get order :ref) "--")))))
+          (progn
+            (byte-recompile-directory repo 0 'force)
+            (require 'elpaca)
+            (and (fboundp 'elpaca-generate-autoloads)
+                 (elpaca-generate-autoloads "elpaca" repo))
+            (kill-buffer buffer))
+        (error "%s" (with-current-buffer buffer (buffer-string))))
     ((error)
-     (delete-directory elpaca-directory 'recursive)
-     (with-current-buffer buffer
-       (goto-char (point-max))
-       (insert (format "\n%S" err))
-       (display-buffer buffer)))))
+     (warn "%s" err)
+     (delete-directory repo 'recursive))))
 (require 'elpaca-autoloads)
 (add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca (elpaca :host github :repo "progfolio/elpaca"))
+(elpaca `(,@elpaca-order))
 (elpaca use-package (require 'use-package))
 (setq package-enable-at-startup nil)
 ;;elpaca end
