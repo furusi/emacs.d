988c8fa6b074e77c3e3c3027b03f4a5e553d2d07
Hiroki Furusho <furusityofu@gmail.com>
2023-11-08 10:35:39 +0900

    move elpaca install code

--- init.el
+++ init.el
@@ -6,7 +6,48 @@
 ;; (require 'profiler)
 ;; (profiler-start 'cpu)
 
-(require 'cl-lib)
+;;elpaca
+(defvar elpaca-installer-version 0.6)
+(defvar elpaca-directory (expand-file-name "elpaca/" (format "%spackages/%s" user-emacs-directory emacs-version)))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+(elpaca use-package (require 'use-package))
+;;elpaca end
+
+
 
 ;; <leaf-install-code>
 (eval-and-compile
@@ -47,6 +88,7 @@
 
 (show-paren-mode t)
 (column-number-mode)
+(require 'cl-lib)
 
 (defcustom my-dropbox-dir (expand-file-name "~/Dropbox")
   "Dropbox directory"
@@ -88,7 +130,7 @@
     (eol-mnemonic-unix . "(LF)")
     (indent-tabs-mode . nil)
     (inhibit-startup-screen . t)
-    (mark-ring-max . 32);; マークの数を32に増やす
+    (mark-ring-max . 128)
     (menu-bar-mode . t)
     (package-user-dir . ,(locate-user-emacs-file (format "elpa/%s" emacs-version)))
     (recentf-auto-cleanup . 'never)
