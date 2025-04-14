3d36fcbbfffc7aed28deed76e632ecd43aab7793
furusi <furusityofu@gmail.com>
2025-04-14 11:04:00 +0900

    elpaca: Update elpaca installer version
    Updated the elpaca-installer-version to 0.11. Additionally, modified
    the autoload loading process by setting load-source-file-function to
    nil within a let binding to ensure that the autoloads are loaded
    correctly without interfering with other configurations.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 ;; (profiler-start 'cpu)
 
 ;;elpaca
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" (format "%spackages/%s" user-emacs-directory emacs-version)))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -42,7 +42,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 (setq elpaca-queue-limit (if (eq system-type 'windows-nt)
